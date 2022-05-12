2e620df6f386a55ef9afed79ed4f457c5cf340dd
Andrés Mappe <afmappe@hotmail.com>
2022-05-11 23:10:10 -0500

    Feature/andy/styles (#3)
    * Se agregaron los estilos generales
    
    * tutorial
    
    * Se agregan los estilos generales
    
    Co-authored-by: Ramon Daniel Echeverri <ramon.p890@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroComponent } from './hero/hero.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
-  declarations: [AppComponent, HeroComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  declarations: [AppComponent, HeroesComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
